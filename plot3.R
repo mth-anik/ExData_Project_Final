1e6b6855bb1ae735c88b589d96f4aabd70e02d84
mth-anik <64371513+mth-anik@users.noreply.github.com>
2020-05-22 12:35:49 +0600

    adding comments

--- plot3.R
+++ plot3.R
@@ -7,15 +7,20 @@
 library(dplyr)
 library(ggplot2)
 
+#reading data
 nei <- readRDS("summarySCC_PM25.rds")
 
+#subsetting the data of baltimore city
 city_dat<-subset(nei,nei$fips==24510)
+#grouping data into packages according to years and summing total emissions of each year
 city_grouped<-group_by(city_dat,type,year)
 total_emissions<-summarise(city_grouped,sum(Emissions))
 names(total_emissions)[3]<-'total_emission'
 
+#plotting with ggplot2 package
 g<-ggplot(data=total_emissions,aes(year,total_emission))
 g+geom_line()+facet_grid(.~type)+scale_x_continuous(breaks=c(1999,2002,2005,2008))+theme(axis.text.x= element_text(angle = 45),axis.text.y= element_text(angle = 45))+labs(title = 'Emission in Baltimore City',y='Total Emission(tons)')
 
+#converting the plot into a png file
 dev.copy(png,file='plot3.png')
 dev.off()
